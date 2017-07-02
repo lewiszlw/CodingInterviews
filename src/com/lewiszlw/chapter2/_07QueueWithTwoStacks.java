902604f7991e6c3e3aec7d32f77620c2b407fd55
lewiszlw <lewiszlw520@gmail.com>
2017-07-02 08:47:52 +0800

    增加异常处理

--- src/com/lewiszlw/chapter2/_07QueueWithTwoStacks.java
+++ src/com/lewiszlw/chapter2/_07QueueWithTwoStacks.java
@@ -10,14 +10,19 @@ import java.util.Stack;
 public class _07QueueWithTwoStacks {
 	public static void main(String[] args) {
 		CQueue queue=new CQueue();
-		queue.appendTail(1);
-		queue.appendTail(2);
-		queue.appendTail(3);
-		System.out.println(queue.deleteHead());
-		System.out.println(queue.deleteHead());
-		queue.appendTail(4);
-		System.out.println(queue.deleteHead());
-		System.out.println(queue.deleteHead());
+		try {
+			queue.appendTail(1);
+			System.out.println(queue.deleteHead());
+			queue.appendTail(2);
+			queue.appendTail(3);
+			System.out.println(queue.deleteHead());
+			System.out.println(queue.deleteHead());
+			queue.appendTail(4);
+			System.out.println(queue.deleteHead());
+			System.out.println(queue.deleteHead());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
 
@@ -27,11 +32,15 @@ class CQueue{
 	public void appendTail(int n){
 		stack1.push(n);
 	}
-	public int deleteHead(){
+	public int deleteHead() throws Exception{
 		//如果stack2不为空，此时若将stack1弹出的值压入stack1，则会乱序
 		if(!stack2.isEmpty()){
 			return stack2.pop();
 		}else {
+			//如果队列为空，则抛出异常
+			if(stack1.size()==0){
+				throw new Exception("queue is empty!");
+			}
 			while(!stack1.isEmpty()){
 				stack2.push(stack1.pop());
 			}
